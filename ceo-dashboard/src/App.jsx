5ebde3d607f4937ec58da10c7d04a7b9bd1c1e63
Tanu123T <162577217+Tanu123T@users.noreply.github.com>
2026-03-10 16:15:02 +0530

    navbar

--- ceo-dashboard/src/App.jsx
+++ ceo-dashboard/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 
