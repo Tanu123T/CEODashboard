abc8f71ed157c9db61bc7d38f822dec66ec6b1d4
Tanu123T <162577217+Tanu123T@users.noreply.github.com>
2026-04-03 13:34:01 +0530

    .....

--- ceodashboard/src/pages/Project/project.jsx
+++ ceodashboard/src/pages/Project/project.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import './Project.css';
+import './project.css';
 import {
   ArrowLeft,
   ArrowRight,
